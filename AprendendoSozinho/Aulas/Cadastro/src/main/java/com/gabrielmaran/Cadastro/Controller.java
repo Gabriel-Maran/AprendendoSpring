16e9479c17d950eabb0e8d3aed3c09115fcf4ca9
Gabriel-Maran <gabriel.enzo.libero.maran@gmail.com>
2025-09-12 11:19:51 -0300

    Reorganizando e aplicando
    Primeiro metodo GetMappging implementado e estrutura de pastas reorganizada para visualização mais facil

---
+++ AprendendoSozinho/Aulas/Cadastro/src/main/java/com/gabrielmaran/Cadastro/Controller.java
@@ -0,0 +1,18 @@
+package com.gabrielmaran.Cadastro;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController//Basicamente diz q esse mano vai ser um controller com api rest
+@RequestMapping//Juntar todas as rotas no mesmo lugar
+public class Controller {
+
+    @GetMapping("/boasvindas")//Esse é uma rota com a requisição GET (Como tbm tem DELETE, PATCH, PUT e POST)
+    public String boasVindas(){
+        return "Bem Vindo!";
+    }
+
+
+
+}
