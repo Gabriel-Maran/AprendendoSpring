f23dc87c32d72d68f7c883b96053fccf3f57134f
Gabriel Maran <gabriel.enzo.libero.maran@gmail.com>
2025-09-23 20:24:41 -0300

    a

--- SalaDeAula/exemplo/src/main/kotlin/com/example/exemplo/contoller/PessoaController.kt
+++ SalaDeAula/exemplo/src/main/kotlin/com/example/exemplo/contoller/PessoaController.kt
@@ -1,11 +1,23 @@
 package com.example.exemplo.contoller
 
+import com.example.exemplo.model.Pessoa
+import com.example.exemplo.repository.PessoaRepository
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 //@Controller //Controller permite ter VIEW, para aplicações web
 @RestController //RestController não pode usar VIEW, mais rapida(para APIs)
-@RequestMapping("/")
-class PessoaController {
+@RequestMapping("/pessoa")
+class PessoaController (
+    private val pessoaRepository: PessoaRepository
+){
+    @PostMapping("/register")
+    fun cadastrarPessoa(@RequestBody pessoa:Pessoa)
+    : ResponseEntity<Pessoa>{
+        return ResponseEntity.ok(pessoaRepository.save<Pessoa>(pessoa))
+    }
 
 }
