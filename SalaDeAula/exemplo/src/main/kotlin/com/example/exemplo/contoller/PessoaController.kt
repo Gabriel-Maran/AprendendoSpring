20969f08881252f46e60c00fb9fd96356c158cfc
Gabriel Maran <gabriel.enzo.libero.maran@gmail.com>
2025-10-21 20:14:07 -0300

    Conexão com front

--- SalaDeAula/exemplo/src/main/kotlin/com/example/exemplo/contoller/PessoaController.kt
+++ SalaDeAula/exemplo/src/main/kotlin/com/example/exemplo/contoller/PessoaController.kt
@@ -3,6 +3,7 @@ package com.example.exemplo.contoller
 import com.example.exemplo.model.Pessoa
 import com.example.exemplo.repository.PessoaRepository
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController
 import java.util.Optional
 
 //@Controller //Controller permite ter VIEW, para aplicações web
+@CrossOrigin(origins = ["http://localhost:5173"])
 @RestController //RestController não pode usar VIEW, mais rapida(para APIs)
 @RequestMapping("/pessoa")
 class PessoaController(
