8b2a24ac4e0a2d9b16d5ff2fa869094cc5d7a59a
Gabriel-Maran <gabriel.enzo.libero.maran@gmail.com>
2026-01-11 20:11:44 -0300

    Content Negoatiation - YAML
    Implementando receber e enviar conteudos .yaml, em GET, POST e PUT

--- AprendendoSozinho/Cursinho/13_Person - Content Negotiation/src/main/java/br/com/gabrielmaran/pessoa/controllers/PersonController.java
+++ AprendendoSozinho/Cursinho/13_Person - Content Negotiation/src/main/java/br/com/gabrielmaran/pessoa/controllers/PersonController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
@@ -15,27 +16,29 @@ public class PersonController {
     @Autowired //Por pura preguiça de fazer um construtor KKKKKKKKK
     private PersonService personService;
 
-    @GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    @GetMapping(
+            value = "/{id}",
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE})
     public ResponseEntity<PersonDTO> findById(@PathVariable("id") Long id) {
         return ResponseEntity.ok(personService.findById(id));
     }
 
-    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE})
     public ResponseEntity<List<PersonDTO>> findAll() {
         return ResponseEntity.ok(personService.findAll());
     }
 
     @PostMapping(
-            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
-            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE}
     )
     public ResponseEntity<PersonDTO> createPerson(@RequestBody PersonDTO pessoa) {
         return ResponseEntity.ok(personService.createPerson(pessoa));
     }
 
     @PutMapping(
-            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
-            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE}
     )
     public ResponseEntity<PersonDTO> updatePerson(@RequestBody PersonDTO person) {
         return ResponseEntity.ok(personService.updatePerson(person));
