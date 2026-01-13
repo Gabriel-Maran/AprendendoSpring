9336f6a7430808ab1b249839ea12225a6a21cc16
Gabriel-Maran <gabriel.enzo.libero.maran@gmail.com>
2026-01-12 21:36:28 -0300

    Aprendendo HATEOAS
    Aprendendo a implementarr hateoas de forma simples, centraliizada e descritiiva

---
+++ AprendendoSozinho/Cursinho/14_Person - HATEOAS/src/main/java/br/com/gabrielmaran/pessoa/service/PersonService.java
@@ -0,0 +1,73 @@
+package br.com.gabrielmaran.pessoa.service;
+
+import br.com.gabrielmaran.pessoa.data.dto.PersonDTO;
+import br.com.gabrielmaran.pessoa.controllers.PersonController;
+import br.com.gabrielmaran.pessoa.exception.ResourceNotFoundException;
+import static br.com.gabrielmaran.pessoa.mapper.ObjectMapper.parseListObjects ;
+import static br.com.gabrielmaran.pessoa.mapper.ObjectMapper.parseObject ;
+import br.com.gabrielmaran.pessoa.model.Person;
+import br.com.gabrielmaran.pessoa.repository.PersonRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PersonService {
+    @Autowired // Por pura preguiça de fazer um contrutor
+    PersonRepository repository;
+
+    private final Logger logger = LoggerFactory.getLogger(PersonService.class.getName());
+
+    public PersonDTO findById(Long id) {
+        logger.info("Finding one person by id: {}", id);
+        var entity =  repository.findById(id).
+                orElseThrow(() -> new ResourceNotFoundException("No records find for " + id + " id"));
+        return addHateoasLinks(parseObject(entity, PersonDTO.class));
+    }
+
+    public List<PersonDTO> findAll() {
+        logger.info("Finding all people");
+        var people = parseListObjects(repository.findAll(), PersonDTO.class);
+        people.forEach(this::addHateoasLinks);
+        return people;
+
+    }
+
+    public PersonDTO createPerson(PersonDTO pessoa) {
+        logger.info("Creating one person");
+        var entity = parseObject(pessoa, Person.class);
+        return addHateoasLinks(parseObject(repository.save(entity), PersonDTO.class));
+    }
+
+    public PersonDTO updatePerson(PersonDTO pessoa) {
+        logger.info("Updating one person");
+        Person entity = repository.findById(pessoa.getId()).
+                orElseThrow(() -> new ResourceNotFoundException("No records find for " + pessoa.getId() + " id"));
+        entity.setFirstName(pessoa.getFirstName());
+        entity.setLastName(pessoa.getLastName());
+        entity.setAddress(pessoa.getAddress());
+        entity.setGender(pessoa.getGender());
+
+        return addHateoasLinks(parseObject(repository.save(entity), PersonDTO.class));
+    }
+
+    public void deletePerson(Long id) {
+        logger.info("Deleting one person");
+        repository.deleteById(id);
+    }
+
+    private PersonDTO addHateoasLinks(PersonDTO person){
+        person.add(linkTo(methodOn(PersonController.class).findById(person.getId())).withSelfRel().withType("GET"));
+        person.add(linkTo(methodOn(PersonController.class).findAll()).withRel("findPeople").withType("GET"));
+        person.add(linkTo(methodOn(PersonController.class).createPerson(person)).withRel("create").withType("POST"));
+        person.add(linkTo(methodOn(PersonController.class).updatePerson(person)).withRel("edit").withType("PUT"));
+        person.add(linkTo(methodOn(PersonController.class).deletePerson(person.getId())).withRel("delete").withType("DELETE"));
+        return person;
+    }
+}
